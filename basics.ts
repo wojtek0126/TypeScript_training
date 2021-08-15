34d55d34b0be07ba4b5e9284ba161d75071eea31
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-08-15 17:11:51 +0200

    Comments added

--- basics.ts
+++ basics.ts
@@ -1,16 +1,26 @@
+// First day of TypeScript. 
+// With it dev can restrict data types etc. for variables, arrays, objects and so on.
+// It is keeping an eye on the code on the go and throw errors wherever orphan data appears,
+// It will prevent mix of data types and these bugs
+
+// declare data types including regex
 let userName: string = "Jack";
 let hasLoggedIn: boolean = true;
 
-userName += "Mieciu";
+userName += "Szczobrzążczyńskikiewiszczyński";
 
 console.log(hasLoggedIn);
 
 let myNumber: number = 777;
 let myRegex: RegExp = /foo/;
 
+// string[] and usage
 const names: string[] = userName.split(" ");
+
+// Array<> and usage
 const myValues: Array<number> = [1, 2, 3, 4];
 
+// interface and usage
 interface Person {
     first: string;
     last: string;
@@ -21,6 +31,7 @@ const myPerson: Person = {
     last: "Marley",
 }
 
+// Record<> and usage
 const ids: Record<number, string> = {
     10: "a",
     20: "b"
@@ -32,10 +43,16 @@ if (ids[30] === "d") {
     
 }
 
+// for loop, no need to declare let i
 for (let i = 0; i < 10; i++) {
     console.log(i + " " + "tu nie trza dodawać : number po let i = 0");
 }
 
+//forEach loop no need to declare number
 [1, 2, 3].forEach((v) => console.log(v));
 
-const out = [4, 5, 6].map((v) => v * 10);
+//number[] for map function restricts numbers only
+const out: number[] = [4, 5, 6].map((v) => v * 10);
+
+// map function without number[] can return string too
+const out2 = [4, 5, 6].map((v) => `${v * 10}`);
